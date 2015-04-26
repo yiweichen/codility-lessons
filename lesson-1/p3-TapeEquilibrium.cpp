dfa9e7c108e508c57ea77dfeb3925f1dd6f85281
Yiwei Chen <yiwei.ch@live.com>
2015-04-26 18:17:15 +0100

    Lesson 1 P3-TapeEquilibrium

---
+++ lesson-1/p3-TapeEquilibrium.cpp
@@ -0,0 +1,24 @@
+// Codility Lesson 1
+// P3: TapeEquilibrium
+
+#include <cmath>
+
+int solution(vector<int> &A) {
+    long left = A[0];
+    long right = 0;
+    for (int i = 1; i < A.size(); i++) {
+        right += A[i];
+    }
+    long minDiff = abs(left - right);
+    long diff;
+    
+    for (int p = 1; p < A.size() - 1; p++) {
+        left += A[p];
+        right -= A[p];
+        diff = abs(left - right);
+        if (diff < minDiff) {
+            minDiff = diff;
+        }
+    }
+    return minDiff;
+}
